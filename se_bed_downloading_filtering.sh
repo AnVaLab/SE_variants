f1b16ce80bafd477c4b500fcc232c48f7d8bc931
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 10:43:28 +0300

    Update se_bed_downloading_filtering.sh

--- se_bed_downloading_filtering.sh
+++ se_bed_downloading_filtering.sh
@@ -2,6 +2,9 @@
 # in SEA SE for all cell lines and tissues for human are gathered in a single bed file
 wget http://218.8.241.248:8080/SEA3/download/SEA00101.bed
 
+# removing first column containing record serial number
+
+
 # filtering records by the regulatory element type, saving SE to a separate file
 awk -F”\t” '{if ($(NF-1)=="SE") print $0}' SEA00101.bed > SE_only_SEA00101.bed
 
