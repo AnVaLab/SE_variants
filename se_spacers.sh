43611779a8a29c89014927becc709012e9e9aa4c
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 10:50:34 +0300

    Create se_spacers.sh

---
+++ se_spacers.sh
@@ -0,0 +1,7 @@
+# Save each SE to a separate file
+sort -k14,14 se_e_filtered.bed | uniq -f 13 --group | awk -v RS="\n\n" '{
+   filename = "/home/avasileva/temp/output_" ++count ".txt"
+   print $0 > filename
+}' 
+
+for filename in /home/avasileva/temp/*; do variable=$(awk 'NR==1 {for (i = 11; i <= NF; i++) {printf "%s\t ", $i}; printf "\n"}' "$filename");  /home/avasileva/programs/bin/bedops --complement  "$filename" | awk  -v var="$variable" '{print $0 "\t.\t.\t.\t.\t.\t.\t.\t" var "SE_S"}' >> se_spacers.bed; done 
