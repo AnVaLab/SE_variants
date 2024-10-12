ab6894607b754c7ec3dda50fd1a1bdfc63babf64
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 10:46:59 +0300

    Update e_bed_downloading_filtering.sh

--- e_bed_downloading_filtering.sh
+++ e_bed_downloading_filtering.sh
@@ -15,20 +15,27 @@ wget https://www.encodeproject.org/files/ENCFF420VPZ/@@download/ENCFF420VPZ.bed.
 gzip -d ENCFF420VPZ.bed.gz
 mv ENCFF420VPZ.bed ENCFF420VPZ_all.bed
 
+### exploring downloaded files
+# types of tags
+awk '{type[$NF]++} END{for (t in type) {print t, type[t]}}' ENCFF420VPZ.bed 
+#PLS 47532
+#CA-H3K4me3 79246
+#CA-TF 26102
+#CA 245985
+#pELS 249464
+#dELS 1469205
+#CA-CTCF 126034
+#TF 105286
+
+# checking if elements in ENCFF900YFA.bed.gz and ENCFF420VPZ.bed.gz overlap exactly
+# number of regulatory elements in ENCFF900YFA_classified.bed file
+cat ENCFF900YFA_monocytes.bed | wc -l
+bedops -e 100% ENCFF420VPZ_all.bed ENCFF900YFA_monocytes.bed | wc -l
+# yes, all elements from file for monocytes exist in file for genome
+
+
 ## filtration
 # filtering out unclassified regulatory elements
 awk -F"\t" '$10!="Unclassified" {print $0}' ENCFF900YFA_monocytes.bed > ENCFF900YFA_monocytes_classified.bed
-
-# filtering out non-enhancers
-# checking if elements in ENCFF900YFA.bed.gz and ENCFF420VPZ.bed.gz overlap exactly
-# number of regulatory elements in ENCFF900YFA_classified.bed file
-cat ENCFF900YFA_monocytes_classified.bed | wc -l
-# 149692
-
-bedops -e 100% ENCFF420VPZ_all.bed ENCFF900YFA_monocytes_classified.bed | wc -l
-# 149692
-
-# yes, all elements from file for monocytes exist in file for genome
-
-# leaving only enhancers
+# leaving only enhancers (filtering out non-enhancers)
 bedops -e 100% ENCFF420VPZ_all.bed ENCFF900YFA_monocytes_classified.bed | grep -E "dELS|pELS" > te_monocytes.bed
