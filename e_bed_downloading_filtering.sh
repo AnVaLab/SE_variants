f1b2d0ebe55169ef7cbffe77f7bfa89b9545b0e1
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 10:33:28 +0300

    Update e_bed_downloading_filtering.sh

--- e_bed_downloading_filtering.sh
+++ e_bed_downloading_filtering.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-# crearing a working folder
+## crearing a working folder
 mkdir ~/project/enhancers
 mkdir ~/project/enhancers/all_regulatory
 
-# downloading bed file for putative regulatory elements in monocytes (not annotated, instead "High H3K27ac/Low H3K27ac)
-wget https://www.encodeproject.org/files/ENCFF420VPZ/@@download/ENCFF420VPZ.bed.gz
+cd  ~/project/enhancers
 
-# downloading bed file with annotated putative regulatory elements in human genome
+### downloading bed files with
+# putative regulatory elements in monocytes (not annotated, instead "High H3K27ac/Low H3K27ac)
+wget https://www.encodeproject.org/files/ENCFF900YFA/@@download/ENCFF900YFA.bed.gz
+gzip -d ENCFF900YFA.bed.gz
+mv ENCFF900YFA.bed ENCFF900YFA_monocytes.bed
+
+# annotated putative regulatory elements in human genome
 wget https://www.encodeproject.org/files/ENCFF420VPZ/@@download/ENCFF420VPZ.bed.gz
 gzip -d ENCFF420VPZ.bed.gz
+mv ENCFF420VPZ.bed ENCFF420VPZ_all.bed
 
+## filtration
 # filtering out unclassified regulatory elements
 awk -F"\t" '$10!="Unclassified" {print $0}' ENCFF900YFA.bed > ENCFF900YFA_classified.bed
 
-# leaving only enhancers
-# checking that regulatory elements found for monocytes overlap exactly regulatory elements with annotated regulatory elements for the entire genome
+# filtering out non-enhancers
+# checking if elements in ENCFF900YFA.bed.gz and ENCFF420VPZ.bed.gz overlap exactly
 # number of regulatory elements in ENCFF900YFA_classified.bed file
 cat ENCFF900YFA_classified.bed | wc -l
 # 149692
@@ -26,4 +33,4 @@ bedops -e 100% ./all_regulatory/ENCFF420VPZ.bed ./ENCFF900YFA_classified.bed | w
 # yes, all elements from file for monocytes exist in file for genome
 
 # leaving only enhancers
-bedops -e 100% ./all_regulatory/ENCFF420VPZ.bed ./ENCFF900YFA_classified.bed | grep -E "dELS|pELS" > monocytes_e_only.bed
+bedops -e 100% ./all_regulatory/ENCFF420VPZ.bed ./ENCFF900YFA_classified.bed | grep -E "dELS|pELS" > te_monocytes.bed
