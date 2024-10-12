5fa1e7b1b1c0a019427e3229c319e3c61fc11f99
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 09:57:57 +0300

    Update se_e_annotation_refinement.sh

--- se_e_annotation_refinement.sh
+++ se_e_annotation_refinement.sh
@@ -3,3 +3,5 @@
 # adding information about enhancers belonging to SE
 ~/programs/bedtools2/bin/bedtools intersect -loj -a ~/project/enhancers/monocytes_e_only.bed \
 -b ~/project/se/SE_only_cd14plus_monocyte_norownumb_SEA00101.bed > ~/project/combined_annotation/enhancers_refined_se_e.bed 
+
+# this will leave information about SE enhancer elements and typical enhancers
