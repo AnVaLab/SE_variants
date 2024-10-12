25ae6ef285fc09928891ab19cf079f164131a450
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 10:50:07 +0300

    Update se_e_annotation_refinement.sh

--- se_e_annotation_refinement.sh
+++ se_e_annotation_refinement.sh
@@ -11,3 +11,34 @@ bedtools intersect -loj -a ~/project/enhancers/te_monocytes.bed \
 # this will leave information about SE enhancer elements and typical enhancers
 
 te_monocytes.bed
+
+# разделяем энхансеры и СЭ в разные файлы, чтобы с ними было проще работать, по отдельности
+awk '$(NF-1)=="SE" {print $0}' /home/avasileva/project/combined_annotation/enhancers_refined_se_e.bed > se_e.bed;
+awk '$(NF-1)!="SE" {print $0}' /home/avasileva/project/combined_annotation/enhancers_refined_se_e.bed > e.bed;
+
+
+# считаем число энхансеров в каждом СЭ
+awk -F"\t" '{se[$(NF-15)]++} END {for (s in se) print s, se[s]}' /home/avasileva/project/combined_annotation/se_e.bed | sort -nk2 > se_elem_numb.txt
+
+# removing SE with only 1 enhancer
+# NR - номер текущей записи
+awk -F"\t" '{
+    count[$(NF-15)]++
+    records[NR] = $0  # Сохраняем каждую запись
+    se_id[NR] = $(NF-15)
+} 
+# здесь NR равно общему числу строк
+END {
+    for (key in count) {
+        if (count[key] > 1) {
+            for (i = 1; i <= NR; i++) {
+                if (se_id[i] == key) {
+                    print records[i]  # Печатаем записи, которые соответствуют критерию
+                }
+            }
+        }
+    }
+}' /home/avasileva/project/combined_annotation/se_e.bed >  /home/avasileva/project/combined_annotation/se_e_filtered.bed
+
+# проверяем результат
+awk -F"\t" '{se[$(NF-15)]++} END {for (s in se) print s, se[s]}' /home/avasileva/project/combined_annotation/se_e_filtered.bed | sort -nk2 | head -10
