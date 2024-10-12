b664c1cd15eaf32f6347d8b70c47db546a3bc06e
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 09:57:09 +0300

    Update se_e_annotation_refinement.sh

--- se_e_annotation_refinement.sh
+++ se_e_annotation_refinement.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# filtering out unclassified enhancers
-awk -F"\t" '$10!="Unclassified" {print $0}' ENCFF900YFA.bed > ENCFF900YFA_classified.bed
-
 # adding information about enhancers belonging to SE
-~/programs/bedtools2/bin/bedtools intersect -loj -a ~/project/enhancers/ENCFF900YFA_classified.bed \
--b ~/project/se/SE_only_cd14plus_monocyte_norownumb_SEA00101.bed > ~/project/enhancers_refined_se_e.bed 
+~/programs/bedtools2/bin/bedtools intersect -loj -a ~/project/enhancers/monocytes_e_only.bed \
+-b ~/project/se/SE_only_cd14plus_monocyte_norownumb_SEA00101.bed > ~/project/combined_annotation/enhancers_refined_se_e.bed 
