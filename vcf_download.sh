a94fe15db683244b72e217ea7a7b38331a7f97e7
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 09:50:35 +0300

    Create vcf_download.sh

---
+++ vcf_download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir /home/avasileva/project/variants/vcf_1000_genomes && cd $_
+
+# Create a file with vcf file download links for selected ids
+awk '{print "https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20190425_NYGC_GATK/raw_calls_updated/" $1 ".haplotypeCalls.er.raw.vcf.gz"}' \
+../samples_meta_male_eur_100_1000Genomes30xGRCh38.csv \
+> download_links_vcf_1000Genomes30xGRCh38.csv
+
+# downloading vcf files
+wget -i download_links_vcf_1000Genomes30xGRCh38.csv
