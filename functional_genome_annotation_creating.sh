fc354654ac4b9b31a6b39670add19db1480fe719
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-12 11:14:20 +0300

    Create functional_genome_annotation_creating.sh

---
+++ functional_genome_annotation_creating.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_21/gencode.v21.annotation.gtf.gz
+gzip -d gencode.v21.annotation.gtf.gz 
+
+# convert gtf annotation to bed format
+/home/avasileva/programs/bin/gtf2bed < gencode.v21.annotation.gtf > gencode.v21.annotation.bed
+
+# filtering annotation
+awk '{type[$8]++} END{for(t in type) print t, type[t]}' gencode.v21.annotation.bed
+
+exon 1162114
+CDS 696929
+UTR 275100
+gene 60155
+start_codon 81862
+Selenocysteine 114
+stop_codon 73993
+transcript 196327
+
+# leaving only gene records
+awk '{if ($8=="gene") {print $0}}' gencode.v21.annotation.bed > genecode.v21.annotation.genes.bed
+
+# merging genome annotation and encode annotation
+bedops --merge /home/avasileva/project/enhancers/all_regulatory/ENCFF420VPZ.bed /home/avasileva/project/hg38/genecode.v21.annotation.genes.bed > reg_elem_gene_ann_merged.bed
