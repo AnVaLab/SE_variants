fe88c4a0a04f256b5b185b485a3bc698c5f92762
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 15:06:00 +0300

    Update and rename se_monocytes_filtering.sh to monocytes_se_filtering.sh

---
+++ monocytes_se_filtering.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## 1. crearing a working folder
+mkdir -p /home/avasileva/project/monocytes/se
+cd  /home/avasileva/project/monocytes/se
+
+## 2. filtering SE only for CD14-positive_monocyte
+awk -F"\t" '$7=="CD14-positive_monocyte" {print $0}' /home/avasileva/project/genome_ann/SE_only_SEA00101_sorted.bed > SE_only_cd14plus_monocyte_SEA00101.bed
