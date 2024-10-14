73695e2d5e8169b4768d4a459e128a771d96c9ad
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 16:37:01 +0300

    Update genome_bg_generation.sh

--- genome_bg_generation.sh
+++ genome_bg_generation.sh
@@ -16,21 +16,3 @@ bedops --chop 268 --stagger 468 -x bg_all.bed > bg_all_chopped.bed
 awk -v OFS="\t" \
 '{print $0 ".\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t" "bg"}' bg_all_chopped.bed > \
 bg_all_fields.bed
-
-
-##########################################################3
-### sub sampling remove from here and add to fnal annotation generation!
-
-## 4. sub-sampling (making bg sample size equal to typical enhancer sample size)
-# checking number of sample elements for TE and bg
-
-# number of elements
-cat bg_all.bed | wc -l
-# 254457
-
-# TE sample size
-cat /home/avasileva/project/monocytes/enhancers/te.bed | wc -l
-# 101634
-
-# select 101634 lines (make sample size equal to the one of enhancers)
-shuf -n 101634  bg_all_chopped.bed >  bg_sampled.bed
