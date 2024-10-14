f3c8df3540c7e172d22366aaea940649232cc54a
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 14:38:57 +0300

    Update genome_sea_downloading_filtering.sh

--- genome_sea_downloading_filtering.sh
+++ genome_sea_downloading_filtering.sh
@@ -2,22 +2,18 @@
 
 # SEA db bed file contains information on SE and E in a number of cell lines, found based on H3K27ac signal (???????????????????????)
 
-### 1. downloading
+## 1. downloading
 # creating a working folder
-mkdir -p /home/avasileva/project/genome_ann/original
-cd /home/avasileva/project/genome_ann/original
+mkdir -p /home/avasileva/project/genome_ann/sea
+cd /home/avasileva/project/genome_ann/sea
 
 # in SEA SE for all cell lines and tissues for human are gathered in a single bed file
 wget http://218.8.241.248:8080/SEA3/download/SEA00101.bed
 
-### 2. processing
-mkdir -p /home/avasileva/project/genome_ann/processed
-cd /home/avasileva/project/genome_ann/processed
+## 2. removing first column containing record serial number
+cut -f2- SEA00101.bed > SEA00101_no_record_numb.bed
 
-## 2.1 removing first column containing record serial number
-cut -f2- /home/avasileva/project/genome_ann/original/SEA00101.bed > SEA00101_no_record_numb.bed
-
-## 2.2 filtering 
+## 3 filtering 
 # filtering records by the regulatory element type, saving SE to a separate file
 awk -F”\t” '{if ($(NF-1)=="SE") print $0}' SEA00101_no_record_numb.bed > SE_only_SEA00101.bed
 # SEA only provides infortmation on start and end coordinates of SEs and does not provide infortmation on SE elements 
