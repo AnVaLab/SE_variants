ca2a00fd94887cfaac572f6f284e1598192fe4c7
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 17:32:45 +0300

    Update monocytes_se_annotation_filtering.sh

--- monocytes_se_annotation_filtering.sh
+++ monocytes_se_annotation_filtering.sh
@@ -5,4 +5,4 @@ mkdir -p /home/avasileva/project/monocytes/se
 cd  /home/avasileva/project/monocytes/se
 
 ## 2. filtering SE only for CD14-positive_monocyte
-awk -F"\t" '$7=="CD14-positive_monocyte" {print $0}' /home/avasileva/project/genome_ann/SE_only_SEA00101_sorted.bed > se.bed
+awk -F"\t" '$7=="CD14-positive_monocyte" {print $0}' /home/avasileva/project/genome_ann/sea/SE_only_SEA00101_sorted.bed > se.bed
