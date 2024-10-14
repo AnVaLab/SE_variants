08aa1d40a4e3b32d4a0359bf2fe32d3335e12bdd
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 16:45:20 +0300

    Update monocytes_se_e_elem_annotation_creating.sh

--- monocytes_se_e_elem_annotation_creating.sh
+++ monocytes_se_e_elem_annotation_creating.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Enhancer elements in SE were defined by intersecting SEA and ENCODE. 
+
 ## 1. creating a working folder
 mkdir -p /home/avasileva/project/monocytes/combined_annotation
 cd /home/avasileva/project/monocytes/combined_annotation
@@ -15,3 +17,35 @@ te_se_e.bed
 ## 3. splitting information on TE and SE E elements into separate files
 awk '$(NF-1)=="SE" {print $0}' te_se_e.bed > /home/avasileva/project/monocytes/se/se_e.bed;
 awk '$(NF-1)!="SE" {print $0}' te_se_e.bed > /home/avasileva/project/monocytes/enhancers/te.bed;
+
+# Since enhancer coordinated differ in SEA and ENCODE inconcistencies were witnessed: in some SE start/end coordinates did not overlap enhancers,
+# some SE concist only out of 1 ENCODE enhancer, which contradicts SE definition. We decided to exclude those SE.
+
+## 4. creating working folder
+mkdir -p /home/avasileva/project/monocytes/se
+cd  /home/avasileva/project/monocytes/se
+
+## 5. counting number of enhancers in each SE
+awk -F"\t" '{se[$(NF-15)]++} END {for (s in se) print s, se[s]}' se_e.bed | sort -nk2 > se_elem_numb.txt
+
+## 6. removing SE with only 1 enhancer
+awk -F"\t" '{
+    count[$(NF-15)]++
+    records[NR] = $0  # Сохраняем каждую запись
+    se_id[NR] = $(NF-15)
+} 
+# here NR equals the total number of records
+END {
+    for (key in count) {
+        if (count[key] > 1) {
+            for (i = 1; i <= NR; i++) {
+                if (se_id[i] == key) {
+                    print records[i]  # Печатаем записи, которые соответствуют критерию
+                }
+            }
+        }
+    }
+}' se_e.bed >  se_e_filtered.bed
+
+# result veridication
+awk -F"\t" '{se[$(NF-15)]++} END {for (s in se) print s, se[s]}' se_e_filtered.bed | sort -nk2 | head -10
