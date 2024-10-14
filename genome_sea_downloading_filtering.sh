03ff5105ae4b62bd1e3a3902582eb0a569f8c864
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 13:42:51 +0300

    Update genome_sea_downloading_filtering.sh

--- genome_sea_downloading_filtering.sh
+++ genome_sea_downloading_filtering.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# SEA db bed file contains information on SE and E in a number of cell lines, found based on H3K27ac signal (???????????????????????)
+
 # creating a working folder
 mkdir -p /home/avasileva/project/genome_ann
 cd /home/avasileva/project/genome_ann
@@ -12,3 +14,8 @@ cut -f2- SEA00101.bed > SEA00101_no_record_numb.bed
 
 # filtering records by the regulatory element type, saving SE to a separate file
 awk -F”\t” '{if ($(NF-1)=="SE") print $0}' SEA00101_no_record_numb.bed > SE_only_SEA00101.bed
+# SEA only provides infortmation on start and end coordinates of SEs and does not provide infortmation on SE elements 
+
+# filtering records by the regulatory element type, saving E to a separate file
+awk -F”\t” '{if ($(NF-1)=="SE") print $0}' SEA00101_no_record_numb.bed > E_only_SEA00101.bed
+# this file further will be used only to compare it with ENCODE elements to veriry E coordinates
