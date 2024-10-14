1041936b9bb8ef0d823cfa6d07f57bf910ee964c
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 13:58:47 +0300

    Update and rename genome_encode_downloading.sh to genome_encode_downloading_sorting.sh

--- genome_encode_downloading_sorting.sh
+++ genome_encode_downloading_sorting.sh
@@ -2,9 +2,10 @@
 
 # ENCODE annotation contains annotated putative regulatory regions of the human genome
 
+## 1. downloading
 # creating a working folder
-mkdir -p /home/avasileva/project/genome_ann
-cd /home/avasileva/project/genome_ann
+mkdir -p /home/avasileva/project/genome_ann/raw
+cd /home/avasileva/project/genome_ann/raw
 
 # downloading annotated putative regulatory elements in human genome from ENCODE
 wget https://www.encodeproject.org/files/ENCFF420VPZ/@@download/ENCFF420VPZ.bed.gz
@@ -22,3 +23,11 @@ awk '{type[$NF]++} END{for (t in type) {print t, type[t]}}' ENCFF420VPZ_all.bed
 #dELS 1469205
 #CA-CTCF 126034
 #TF 105286
+
+## 2. sorting 
+# creating a working folder
+mkdir -p /home/avasileva/project/genome_ann/sorted
+cd /home/avasileva/project/genome_ann/sorted
+
+# sorting
+bedtools sort -i ENCFF420VPZ_all.bed > ENCFF420VPZ_all_sorted.bed
