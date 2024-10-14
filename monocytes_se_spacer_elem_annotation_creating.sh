db62787f51246fdb4429445e029a31b693fadd9c
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 15:22:14 +0300

    Update monocytes_se_spacer_elem_annotation_creating.sh

--- monocytes_se_spacer_elem_annotation_creating.sh
+++ monocytes_se_spacer_elem_annotation_creating.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# creating a working dirrectory
+## 1. creating a working dirrectory
 mkdir /home/avasileva/project/se/spacers
 cd /home/avasileva/project/se/spacers
 
-# Save each SE to a separate file
+## 2. saving each SE to a separate file
 sort -k14,14 se_e_filtered.bed | uniq -f 13 --group | awk -v RS="\n\n" '{
    filename = "/home/avasileva/temp/output_" ++count ".txt"
    print $0 > filename
 }' 
 
+## 3. creating file with complement (SE spacers)
 for filename in /home/avasileva/temp/*; \
 do \
 variable=$(awk 'NR==1 {for (i = 11; i <= NF; i++) {printf "%s\t ", $i}; printf "\n"}' "$filename");  \
@@ -17,3 +18,5 @@ bedops --complement  "$filename" | \
 awk  -v var="$variable" '{print $0 "\t.\t.\t.\t.\t.\t.\t.\t" var "SE_S"}' >> \
 se_spacers.bed; \
 done 
+
+## 4. annotating spacers (SE spacers overlap with genes and regulatory elements, this information is important to consider in variation alalysis)
