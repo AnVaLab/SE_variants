7b5592e28e7bc1eda112d8bc948a8b1ca2c148d4
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 14:15:59 +0300

    Update genome_hg38_downloading_filtering.sh

--- genome_hg38_downloading_filtering.sh
+++ genome_hg38_downloading_filtering.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## 1. downloading hg38 genome annotation
+### 1. downloading hg38 genome annotation
 # creating a working folder
 mkdir -p /home/avasileva/project/genome_ann/original
 cd /home/avasileva/project/genome_ann/original
@@ -9,11 +9,12 @@ cd /home/avasileva/project/genome_ann/original
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_21/gencode.v21.annotation.gtf.gz
 gzip -d gencode.v21.annotation.gtf.gz 
 
-## 2. converting gtf annotation to bed format
+### 2. processing
 # creating a working folder
 mkdir -p /home/avasileva/project/genome_ann/processed
 cd /home/avasileva/project/genome_ann/processed
 
+## 2.1 converting gtf annotation to bed format
 gtf2bed < /home/avasileva/project/genome_ann/original/gencode.v21.annotation.gtf > gencode.v21.annotation.bed
 
 # exploring genome annotation
@@ -27,10 +28,9 @@ awk '{type[$8]++} END{for(t in type) print t, type[t]}' gencode.v21.annotation.b
 #stop_codon 73993
 #transcript 196327
 
-## 3. filtering
-# filtering genome annotaion
-# leaving only gene records
+## 2.2 filtering
+# filtering genome annotaion, leaving only gene records
 awk '{if ($8=="gene") {print $0}}' gencode.v21.annotation.bed > genecode.v21.annotation.genes.bed
 
-## 4. sorting 
+## 2.3 sorting 
 bedtools sort -i genecode.v21.annotation.genes.bed > genecode.v21.annotation.genes_sorted.bed
