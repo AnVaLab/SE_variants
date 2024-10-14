9cabb90a6d66adfea810f52c3106631df197ceb8
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 16:42:56 +0300

    Update monocytes_se_e_elem_annotation_creating.sh

--- monocytes_se_e_elem_annotation_creating.sh
+++ monocytes_se_e_elem_annotation_creating.sh
@@ -7,11 +7,11 @@ cd /home/avasileva/project/monocytes/combined_annotation
 ## 2. intersecting encode and sea annotations to determine SE enhancer elements
 bedtools intersect \
 -loj \
--a /home/avasileva/project/monocytes/enhancers/e_monocytes.bed \
--b /home/avasileva/project/monocytes/se/SE_only_cd14plus_monocyte_SEA00101.bed > \
+-a /home/avasileva/project/monocytes/enhancers/e.bed \
+-b /home/avasileva/project/monocytes/se/se.bed > \
 te_se_e.bed 
 # this file contains information about SE enhancer elements and typical enhancers
 
 ## 3. splitting information on TE and SE E elements into separate files
-awk '$(NF-1)=="SE" {print $0}' /home/avasileva/project/monocytes/combined_annotation/te_se_e.bed > /home/avasileva/project/monocytes/se/se_e.bed;
-awk '$(NF-1)!="SE" {print $0}' /home/avasileva/project/monocytes/combined_annotation/te_se_e.bed > /home/avasileva/project/monocytes/enhancers/te.bed;
+awk '$(NF-1)=="SE" {print $0}' te_se_e.bed > /home/avasileva/project/monocytes/se/se_e.bed;
+awk '$(NF-1)!="SE" {print $0}' te_se_e.bed > /home/avasileva/project/monocytes/enhancers/te.bed;
