a9de712d5296978e08fe705b64f636785521fd1b
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 15:04:53 +0300

    Update and rename se_e_elem_annotation_creating.sh to monocytes_se_e_elem_annotation_creating.sh

--- monocytes_se_e_elem_annotation_creating.sh
+++ monocytes_se_e_elem_annotation_creating.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# creating a working folder
-mkdir ~/project/combined_annotation
-cd ~/project/combined_annotation
+## 1. creating a working folder
+mkdir -p /home/avasileva/project/combined_annotation
+cd /home/avasileva/project/combined_annotation
 
-# adding information about enhancers belonging to SE
-bedtools intersect -loj -a ~/project/enhancers/te_monocytes.bed \
--b ~/project/se/SE_only_cd14plus_monocyte_norownumb_SEA00101.bed > enhancers_refined_se_e.bed 
+## 2. intersecting encode and sea annotations to determine SE enhancer elements
+bedtools intersect \
+-loj \
+-a /home/avasileva/project/monocytes/enhancers/e_monocytes.bed \
+
+# исправить на нужный файл
+-b /home/avasileva/project/monocytes/SE_only_cd14plus_monocyte_norownumb_SEA00101.bed > \
+enhancers_refined_se_e.bed 
 
 # this will leave information about SE enhancer elements and typical enhancers
 
