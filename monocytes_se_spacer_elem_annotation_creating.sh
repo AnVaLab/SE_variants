8948975474a0ca69082d59523542ac2d5e220cd3
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 15:27:32 +0300

    Update monocytes_se_spacer_elem_annotation_creating.sh

--- monocytes_se_spacer_elem_annotation_creating.sh
+++ monocytes_se_spacer_elem_annotation_creating.sh
@@ -20,3 +20,13 @@ se_spacers.bed; \
 done 
 
 ## 4. annotating spacers (SE spacers overlap with genes and regulatory elements, this information is important to consider in variation alalysis)
+
+# sort file
+~/programs/bedtools2/bin/bedtools sort -i /home/avasileva/project/combined_annotation/se_spacers.bed > /home/avasileva/project/combined_annotation/se_spacers_sorted.bed
+
+bedtools  intersect  -b /home/avasileva/project/combined_annotation/se_spacers_sorted.bed -a /home/avasileva/project/hg38/genecode.v21.annotation.genes_sorted.bed > intersected_ann_spacers.bed
+
+# intersecting spacers with regulatory regions
+bedtools  intersect  -b /home/avasileva/project/combined_annotation/se_spacers_sorted.bed -a /home/avasileva/project/enhancers/all_regulatory/ENCFF420VPZ.bed > intersected_reg_spacers.bed
+
+####### finish and check the result in igv
