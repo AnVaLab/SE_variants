dc96d3c8d31b9783306565257d85458fe8a05204
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 12:17:14 +0300

    Update e_bed_downloading_filtering.sh

--- e_bed_downloading_filtering.sh
+++ e_bed_downloading_filtering.sh
@@ -1,41 +1,27 @@
 #!/bin/bash
 
 ## crearing a working folder
-mkdir ~/project/enhancers
-cd  ~/project/enhancers
+mkdir -p /home/avasileva/project/enhancers
+cd  /home/avasileva/project/enhancers
 
-### downloading bed files with
-# putative regulatory elements in monocytes (not annotated, instead "High H3K27ac/Low H3K27ac)
+# downloading ENCODE annotation of putative regulatory elements in monocytes (pre not annotated, instead "High H3K27ac/Low H3K27ac)
 wget https://www.encodeproject.org/files/ENCFF900YFA/@@download/ENCFF900YFA.bed.gz
 gzip -d ENCFF900YFA.bed.gz
 mv ENCFF900YFA.bed ENCFF900YFA_monocytes.bed
 
-# annotated putative regulatory elements in human genome
-wget https://www.encodeproject.org/files/ENCFF420VPZ/@@download/ENCFF420VPZ.bed.gz
-gzip -d ENCFF420VPZ.bed.gz
-mv ENCFF420VPZ.bed ENCFF420VPZ_all.bed
+## filtration
+# filtering out unclassified regulatory elements
+awk -F"\t" '$10!="Unclassified" {print $0}' ENCFF900YFA_monocytes.bed > ENCFF900YFA_monocytes_classified.bed
 
-### exploring downloaded files
-# types of tags
-awk '{type[$NF]++} END{for (t in type) {print t, type[t]}}' ENCFF420VPZ.bed 
-#PLS 47532
-#CA-H3K4me3 79246
-#CA-TF 26102
-#CA 245985
-#pELS 249464
-#dELS 1469205
-#CA-CTCF 126034
-#TF 105286
+# leaving only enhancers (filtering out non-enhancers)
+# ENCFF900YFA.bed contains PRE without annotation, and therefore, aside from enhancers, it can include promoters, TE binding sites etc.
+# to leave only enhancers, we will use ENCFF420VPZ_all.bed file containing annotated PRE of all human genome (where each PRE has a tag: dELS, pELS, CTCF etc)
 
-# checking if elements in ENCFF900YFA.bed.gz and ENCFF420VPZ.bed.gz overlap exactly
+# checking if ENCFF420VPZ_all.bed (annotation of all putative regulatory elements of human genome) contains PRE of ENCFF900YFA.bed (PRE in monocytes)
 # number of regulatory elements in ENCFF900YFA_classified.bed file
 cat ENCFF900YFA_monocytes.bed | wc -l
 bedops -e 100% ENCFF420VPZ_all.bed ENCFF900YFA_monocytes.bed | wc -l
 # yes, all elements from file for monocytes exist in file for genome
 
-
-## filtration
-# filtering out unclassified regulatory elements
-awk -F"\t" '$10!="Unclassified" {print $0}' ENCFF900YFA_monocytes.bed > ENCFF900YFA_monocytes_classified.bed
-# leaving only enhancers (filtering out non-enhancers)
+# leaving only distal and proximal enhancer like sites
 bedops -e 100% ENCFF420VPZ_all.bed ENCFF900YFA_monocytes_classified.bed | grep -E "dELS|pELS" > te_monocytes.bed
