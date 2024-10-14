a21ab1f8af62dcbd0db27904843b5637b3e4cdcd
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 16:39:57 +0300

    Update monocytes_se_e_elem_annotation_creating.sh

--- monocytes_se_e_elem_annotation_creating.sh
+++ monocytes_se_e_elem_annotation_creating.sh
@@ -13,5 +13,5 @@ te_se_e.bed
 # this file contains information about SE enhancer elements and typical enhancers
 
 ## 3. splitting information on TE and SE E elements into separate files
-awk '$(NF-1)=="SE" {print $0}' /home/avasileva/project/combined_annotation/te_se_e.bed > /home/avasileva/project/se/se_e.bed;
-awk '$(NF-1)!="SE" {print $0}' /home/avasileva/project/combined_annotation/te_se_e.bed > /home/avasileva/project/enhancers/te.bed;
+awk '$(NF-1)=="SE" {print $0}' /home/avasileva/project/monocytes/combined_annotation/te_se_e.bed > /home/avasileva/project/monocytes/se/se_e.bed;
+awk '$(NF-1)!="SE" {print $0}' /home/avasileva/project/monocytes/combined_annotation/te_se_e.bed > /home/avasileva/project/monocytes/enhancers/te.bed;
