5741627bf0bb8246d7f9f101d964f9d8a0b27e33
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 15:11:10 +0300

    Update monocytes_se_e_elem_annotation_creating.sh

--- monocytes_se_e_elem_annotation_creating.sh
+++ monocytes_se_e_elem_annotation_creating.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 ## 1. creating a working folder
-mkdir -p /home/avasileva/project/combined_annotation
-cd /home/avasileva/project/combined_annotation
+mkdir -p /home/avasileva/project/monocytes/combined_annotation
+cd /home/avasileva/project/monocytes/combined_annotation
 
 ## 2. intersecting encode and sea annotations to determine SE enhancer elements
 bedtools intersect \
 -loj \
 -a /home/avasileva/project/monocytes/enhancers/e_monocytes.bed \
+-b /home/avasileva/project/monocytes/se/SE_only_cd14plus_monocyte_SEA00101.bed > \
+te_se_e.bed 
+# this file contains information about SE enhancer elements and typical enhancers
 
-# исправить на нужный файл
--b /home/avasileva/project/monocytes/SE_only_cd14plus_monocyte_norownumb_SEA00101.bed > \
-enhancers_refined_se_e.bed 
-
-# this will leave information about SE enhancer elements and typical enhancers
-
-# разделяем энхансеры и СЭ в разные файлы, чтобы с ними было проще работать, по отдельности
-awk '$(NF-1)=="SE" {print $0}' /home/avasileva/project/combined_annotation/enhancers_refined_se_e.bed > /home/avasileva/project/se/se_e_elements.bed;
-awk '$(NF-1)!="SE" {print $0}' /home/avasileva/project/combined_annotation/enhancers_refined_se_e.bed > /home/avasileva/project/enhancers/te_only.bed;
+## 3. splitting information on TE and SE E elements into separate files
+awk '$(NF-1)=="SE" {print $0}' /home/avasileva/project/combined_annotation/te_se_e.bed > /home/avasileva/project/se/se_e.bed;
+awk '$(NF-1)!="SE" {print $0}' /home/avasileva/project/combined_annotation/te_se_e.bed > /home/avasileva/project/enhancers/te.bed;
