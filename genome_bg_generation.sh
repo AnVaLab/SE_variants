b11f0597b5d8e82690061753cbcfb67557c13fd1
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 15:49:48 +0300

    Update genome_bg_generation.sh

--- genome_bg_generation.sh
+++ genome_bg_generation.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# finding compliment 
-bedops --complement /home/avasileva/project/combined_annotation/reg_elem_gene_ann_se_merged.bed > bg_all.bed
+## 1. creating a working folder
+mkdir -p /home/avasileva/project/genome_ann/bg
+cd /home/avasileva/project/genome_ann/bg
+
+## 2. finding compliment 
+bedops --complement /home/avasileva/project/genome_ann/merged/hg38_encode_sea_ann_merged.bed > bg_all.bed
+
+## 3. chopping compliment to pieces with length equal to average typical enhancer length
+bedops --chop 268 --stagger 468 -x bg_all.bed > bg_all_chopped.bed
+
+## 4. sub-sampling (making bg sample size equal to typical enhancer sample size)
+# checking number of sample elements for TE and bg
 
 # number of elements
 cat bg_all.bed | wc -l
-254457
+# 254457
 
-# making sample size equal to enhancer sample size
-cat e.bed | wc -l
-101634
-
-# chopping compliment to pieces with length equal to average enhancer length
-bedops --chop 268 --stagger 468 -x bg_all.bed > bg_chopped.bed
+# TE sample size
+cat /home/avasileva/project/monocytes/enhancers/e.bed | wc -l
+# 101634
 
 # select 101634 lines (make sample size equal to the one of enhancers)
-shuf -n 101634  bg_chopped.bed >  bg_chopped_sampled.bed
+shuf -n 101634  bg_all_chopped.bed >  bg_sampled.bed
 
-# add file to annotation
-# add extra columns
-awk -v OFS="\t" '{print $0 ".\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t" "bg"}' bg_chopped_sampled.bed > bg_chopped_sampled_all_fields.bed
+## 5. unifying bed file (so it all fields are the same as in other annotation files) 
+awk -v OFS="\t" \
+'{print $0 ".\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t" "bg"}' bg_sampled.bed > \
+bg_sampled_all_fields.bed
