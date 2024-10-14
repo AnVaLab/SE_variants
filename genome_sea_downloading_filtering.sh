cb876f522d7fd82e29e97633a6a8b048ade21f7a
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-14 17:44:31 +0300

    Update genome_sea_downloading_filtering.sh

--- genome_sea_downloading_filtering.sh
+++ genome_sea_downloading_filtering.sh
@@ -12,16 +12,14 @@ wget http://218.8.241.248:8080/SEA3/download/SEA00101.bed
 
 ## 3. removing first column containing record serial number
 cut -f2- SEA00101.bed > SEA00101_no_record_numb.bed
-#########################3 check!!!!!!!!!!!!!!!1 is it the 1st column or the second that was removed??????
-
 
 ## 4 filtering 
 # filtering records by the regulatory element type, saving SE to a separate file
-awk -F”\t” '{if ($(NF-1)=="SE") print $0}' SEA00101_no_record_numb.bed > SE_only_SEA00101.bed
+awk -F"\t" '{if ($(NF-1)=="SE") print $0}' SEA00101_no_record_numb.bed > SE_only_SEA00101.bed
 # SEA only provides infortmation on start and end coordinates of SEs and does not provide infortmation on SE elements 
 
 # filtering records by the regulatory element type, saving E to a separate file
-awk -F”\t” '{if ($(NF-1)=="E") print $0}' SEA00101_no_record_numb.bed > TE_only_SEA00101.bed
+awk -F"\t" '{if ($(NF-1)=="E") print $0}' SEA00101_no_record_numb.bed > TE_only_SEA00101.bed
 # this file further will be used only to compare it with ENCODE elements to veriry E coordinates
 
 ## 5 sorting
