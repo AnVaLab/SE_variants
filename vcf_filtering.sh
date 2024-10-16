81f4df87f4765b2c52bdc5cd6bbb5de7dd184a56
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-17 00:58:08 +0300

    Update vcf_filtering.sh

--- vcf_filtering.sh
+++ vcf_filtering.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-mkdir ~/project/variants/vcf_1000_genomes_filtered
-
+mkdir -p ~/project/variants/vcf_1000_genomes_filtered
 cd ~/project/variants/vcf_1000_genomes/
-vcf_files=(*)
-echo ${vcf_files[@]}
+
+ls *.vcf.gz | parallel -j 7 'bedtools intersect -wa -wb \
+-a ~/project/variants/vcf_1000_genomes/{} \
+-b /home/avasileva/project/monocytes/combined_annotation/combined_annotation.bed > \
+~/project/variants/vcf_1000_genomes_filtered/filtered_{}'
+
+
+
+
+
 
 # intersecting vcf and bed files
 # leaving only mutations in SE enhancer elements (taken from ENCODE)
@@ -12,6 +19,6 @@ for i in "${vcf_files[@]}"; \
 do \
 ~/programs/bedtools2/bin/bedtools intersect -wa -wb \
 -a ~/project/variants/vcf_1000_genomes/$i \
--b ~/project/enhancers_refined_se_e.bed > \
-~/project/variants/vcf_1000_genomes_filtered/vcf_1000_genomes_filtered_se/${i%.vcf*}_filtered_se.vcf; \
+-b /home/avasileva/project/monocytes/combined_annotation/combined_annotation.bed > \
+~/project/variants/vcf_1000_genomes_filtered/${i%.vcf*}_filtered.vcf; \
 done;
