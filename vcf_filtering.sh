2a362099427dc9f997b32212dadb944a324e8aca
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-17 22:39:31 +0300

    Update vcf_filtering.sh

--- vcf_filtering.sh
+++ vcf_filtering.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 mkdir -p ~/project/variants/vcf_1000_genomes_filtered
-cd ~/project/variants/vcf_1000_genomes/
+cd ~/project/variants/vcf_1000_genomes_filtered/
 
 ls *.vcf.gz | parallel -j 7 'bedtools intersect -wa -wb \
 -a ~/project/variants/vcf_1000_genomes/{} \
 -b /home/avasileva/project/monocytes/combined_annotation/combined_annotation.bed > \
 ~/project/variants/vcf_1000_genomes_filtered/filtered_{}'
+
+
+mkdir -p ~/project/variants/vcf_1000_genomes_filtered/vcf_1000_genomes_filtered_header
+cd ~/project/variants/vcf_1000_genomes_filtered/
+
+# appending headers
+ls *.vcf | \
+parallel -j 7 'id=$(echo {} | \
+grep -oP '(?<=filtered_).*?(?=\.haplotype)'); \
+zcat ~/project/variants/vcf_1000_genomes/"${id}.haplotypeCalls.er.raw.vcf.gz" | \
+awk '{if (/^#/) print; else exit}' |\
+cat -  ~/project/variants/vcf_1000_genomes_filtered/"filtered_${id}.haplotypeCalls.er.raw.vcf" > \
+~/project/variants/vcf_1000_genomes_filtered/"vcf_1000_genomes_filtered_header/filtered_header_${id}.haplotypeCalls.er.raw.vcf"'
 
 
 
