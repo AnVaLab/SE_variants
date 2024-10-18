d60cec6752dc293ab8eeb8bad6f408b1909dd962
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-18 13:07:18 +0300

    Update vcf_filtering.sh

--- vcf_filtering.sh
+++ vcf_filtering.sh
@@ -3,23 +3,45 @@
 mkdir -p ~/project/variants/vcf_1000_genomes_filtered
 cd ~/project/variants/vcf_1000_genomes_filtered/
 
-ls *.vcf.gz | parallel -j 7 'bedtools intersect -wa -wb \
+ls *.vcf.gz | \
+parallel -j 7 '\
+bedtools intersect -wa -wb \
 -a ~/project/variants/vcf_1000_genomes/{} \
 -b /home/avasileva/project/monocytes/combined_annotation/combined_annotation.bed > \
 ~/project/variants/vcf_1000_genomes_filtered/filtered_{}'
+
+cd ~/project/variants/vcf_1000_genomes_filtered/
+
+# removing lines with <ref only>
+ls *.vcf |
+parallel -j 7 "\
+grep -ivP '\t<NON_REF>\t' {} > {}.temp; \
+mv {}.tmp {}
+"
 
 
 mkdir -p ~/project/variants/vcf_1000_genomes_filtered/vcf_1000_genomes_filtered_header
 cd ~/project/variants/vcf_1000_genomes_filtered/
 
 # appending headers
-ls *.vcf | \
-parallel -j 7 'id=$(echo {} | \
-grep -oP '(?<=filtered_).*?(?=\.haplotype)'); \
-zcat ~/project/variants/vcf_1000_genomes/"${id}.haplotypeCalls.er.raw.vcf.gz" | \
-awk '{if (/^#/) print; else exit}' |\
-cat -  ~/project/variants/vcf_1000_genomes_filtered/"filtered_${id}.haplotypeCalls.er.raw.vcf" > \
-~/project/variants/vcf_1000_genomes_filtered/"vcf_1000_genomes_filtered_header/filtered_header_${id}.haplotypeCalls.er.raw.vcf"'
+ls *.vcf |  parallel -j 8 --plus "\
+zcat ~/project/variants/vcf_1000_genomes/{#filtered_}.gz | \
+awk '{if (\$0~/^#/) {print} else exit}' |
+cat - ~/project/variants/vcf_1000_genomes_filtered/{}
+> \
+~/project/variants/vcf_1000_genomes_filtered_header/"vcf_1000_genomes_filtered_header/header_{}'
+"
+
+
+
+
+ls *.vcf | 
+parallel -j 4 --plus "\
+zcat ~/project/variants/vcf_1000_genomes/{#filtered_}.gz | \
+awk '$0~/^#/ {print}'" |\
+cat - ~/project/variants/vcf_1000_genomes_filtered/{}
+> \
+~/project/variants/vcf_1000_genomes_filtered/"vcf_1000_genomes_filtered_header/header_{}'
 
 
 
