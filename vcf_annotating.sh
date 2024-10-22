d8aef1d159239f5b52451e6a573e5232ce5badc8
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-22 20:56:52 +0300

    Update vcf_annotating.sh

--- vcf_annotating.sh
+++ vcf_annotating.sh
@@ -11,19 +11,14 @@ screen -S annotation
 ls *.vcf |
 parallel -j 50 "\
 java -Xmx8g -jar /home/avasileva/programs/snpEff/snpEff.jar \
--c /home/avasileva/programs/snpEff/snpEff.config -v hg38 {} > \
-/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated/{.}.ann.vcf"
+-c /home/avasileva/programs/snpEff/snpEff.config \
+-stats snpeff_{.} \
+-v hg38 {} > \
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated/{.}.ann.vcf &&
+"
 
 # разнести доп поля и vcf файл в разные файлы во время аннотации а потом объединить
 
-
-
-grep -ivP '\t<NON_REF>\t' {} > {}.temp && \
-mv {}.temp {} && \
-echo '{} completed'
-"
-
-java -Xmx8g -jar snpEff.jar GRCh37.75 examples/test.chr22.vcf > test.chr22.ann.vcf
 
 
 # downloading dbs
