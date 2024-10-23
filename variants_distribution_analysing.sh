35b586be8d432d9efdcd17f36e9dafd470ee98cb
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-23 23:59:21 +0300

    Update variants_distribution_analysing.sh

--- variants_distribution_analysing.sh
+++ variants_distribution_analysing.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+#!/bin/bash
 
-разбить общую аннотацию на куски по 10 нуклеотидов, сохранив информацию с айдишниками
+# разбить общую аннотацию на куски по 10 нуклеотидов
+bedops --chop 10 /home/avasileva/project/genome_ann/bg/bg_sampled.bed > /home/avasileva/project/genome_ann/bg/bg_sampled_chopped.bed
+bedops --chop 10 /home/avasileva/project/monocytes/se/se_e_filtered.bed > /home/avasileva/project/monocytes/se/se_e_filtered_chopped.bed
+bedops --chop 10  /home/avasileva/project/monocytes/enhancers/te.bed >  /home/avasileva/project/monocytes/enhancers/te_chopped.bed
+
+
+mkdir -p /home/avasileva/project/variant/variant_distribution
+ls *.vcf |
+parallel -j 50 "\
+bedtools intersect \
+-a /home/avasileva/project/genome_ann/bg/bg_sampled.bed \
+-b {} \
+-c > /home/avasileva/project/variant/variant_distribution/{}
+"
+
+bedtools intersect \
+-a /home/avasileva/project/genome_ann/bg/bg_sampled.bed \
+-b {} \
+-c > /home/avasileva/project/variant/variant_distribution/{}
+
+
+ /home/avasileva/project/genome_ann/bg/bg_sampled.bed
+ /home/avasileva/project/monocytes/se/se_e_filtered.bed
+ /home/avasileva/project/monocytes/enhancers/te.bed
+
+ 
+
 
 потом использовать комманду интерсект
 
