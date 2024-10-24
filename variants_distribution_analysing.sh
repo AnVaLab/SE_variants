a594c45dd02fb8eb991ef79e53f0a8cc7e96a987
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-24 09:55:27 +0300

    Update variants_distribution_analysing.sh

--- variants_distribution_analysing.sh
+++ variants_distribution_analysing.sh
@@ -3,16 +3,22 @@
 #!/bin/bash
 
 # разбить общую аннотацию на куски по 10 нуклеотидов
-bedops --chop 10 /home/avasileva/project/genome_ann/bg/bg_sampled.bed > /home/avasileva/project/genome_ann/bg/bg_sampled_chopped.bed
-bedops --chop 10 /home/avasileva/project/monocytes/se/se_e_filtered.bed > /home/avasileva/project/monocytes/se/se_e_filtered_chopped.bed
+bedops --chop 10 /home/avasileva/project/genome_ann/bg/bg_sampled.bed > /home/avasileva/project/genome_ann/bg/bg_sampled_chopped.bed;
+bedops --chop 10 /home/avasileva/project/monocytes/se/se_e_filtered.bed > /home/avasileva/project/monocytes/se/se_e_filtered_chopped.bed;
 bedops --chop 10  /home/avasileva/project/monocytes/enhancers/te.bed >  /home/avasileva/project/monocytes/enhancers/te_chopped.bed
 
 
+ls *.vcf.gz |
+parallel -j 100 "\
+mv {} {.}
+"
+
 mkdir -p /home/avasileva/project/variant/variant_distribution
-ls *.vcf |
-parallel -j 50 "\
+cd vcf_1000_genomes_filtered_no_no_ref_fields
+ls *.vcf.gz |
+parallel -j 100 "\
 bedtools intersect \
--a /home/avasileva/project/genome_ann/bg/bg_sampled.bed \
+-a /home/avasileva/project/genome_ann/bg/bg_sampled_chopped.bed \
 -b {} \
 -c > /home/avasileva/project/variant/variant_distribution/{}
 "
