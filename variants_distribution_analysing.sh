565778a77a183b26bf862f9f83505186e5ff48a3
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-24 10:05:00 +0300

    Update variants_distribution_analysing.sh

--- variants_distribution_analysing.sh
+++ variants_distribution_analysing.sh
@@ -7,41 +7,36 @@ bedops --chop 10 /home/avasileva/project/genome_ann/bg/bg_sampled.bed > /home/av
 bedops --chop 10 /home/avasileva/project/monocytes/se/se_e_filtered.bed > /home/avasileva/project/monocytes/se/se_e_filtered_chopped.bed;
 bedops --chop 10  /home/avasileva/project/monocytes/enhancers/te.bed >  /home/avasileva/project/monocytes/enhancers/te_chopped.bed
 
-
-ls *.vcf.gz |
-parallel -j 100 "\
-mv {} {.}
-"
-
-mkdir -p /home/avasileva/project/variant/variant_distribution
-cd vcf_1000_genomes_filtered_no_no_ref_fields
-ls *.vcf.gz |
+# counting number of variants per 10 np
+mkdir -p /home/avasileva/project/variants/variant_distribution/bg
+cd /home/avasileva/project/variants/vcf_1000_genomes_filtered_no_no_ref_fields_header
+ls *.vcf |
 parallel -j 100 "\
 bedtools intersect \
 -a /home/avasileva/project/genome_ann/bg/bg_sampled_chopped.bed \
 -b {} \
--c > /home/avasileva/project/variant/variant_distribution/{}
+-c > /home/avasileva/project/variants/variant_distribution/bg{}
 "
 
+mkdir mkdir -p /home/avasileva/project/variants/variant_distribution/se_e
+ls *.vcf |
+parallel -j 100 "\
 bedtools intersect \
--a /home/avasileva/project/genome_ann/bg/bg_sampled.bed \
+-a  /home/avasileva/project/monocytes/se/se_e_filtered_chopped.bed \
 -b {} \
--c > /home/avasileva/project/variant/variant_distribution/{}
+-c > /home/avasileva/project/variants/variant_distribution/se_e{}
+"
+
+mkdir mkdir -p /home/avasileva/project/variants/variant_distribution/te
+ls *.vcf |
+parallel -j 100 "\
+bedtools intersect \
+-a   /home/avasileva/project/monocytes/enhancers/te_chopped.bed \
+-b {} \
+-c > /home/avasileva/project/variants/variant_distribution/se_e{}
+"
 
 
- /home/avasileva/project/genome_ann/bg/bg_sampled.bed
- /home/avasileva/project/monocytes/se/se_e_filtered.bed
- /home/avasileva/project/monocytes/enhancers/te.bed
-
- 
-
-
-потом использовать комманду интерсект
 
 найти корреляцию спирмана по пациетам
 
-
-
-
-
-$ bedtools intersect -a A.bed -b B.bed -c
