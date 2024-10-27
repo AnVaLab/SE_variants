b1b285cb37996f7c585e55883d6ca63939a3f244
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-10-27 16:56:48 +0300

    Update vcf_annotating.sh

--- vcf_annotating.sh
+++ vcf_annotating.sh
@@ -60,12 +60,34 @@ java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate \
 mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp
 screen -S annotation
 ls *.vcf |
-parallel -j 8 "\
-java -Xmx8g -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
+parallel -j 2 "\
+java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
  /home/avasileva/project/variants/db/dbsnp/00-All.vcf {} > \
 /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp/dbsnp_{.}.ann.vcf
 "
 
+mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad
+
+ls *.vcf |
+parallel -j 2 "\
+for i in {1..22};
+do
+java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
+/home/avasileva/project/variants/db/gnomad/gnomad.genomes.v4.1.sites.chr${i}.vcf.bgz {} > \
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp
+mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/{}
+done
+
+java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
+/home/avasileva/project/variants/db/gnomad/gnomad.genomes.v4.1.sites.chrX.vcf.bgz {} > \
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp
+mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/{}
+
+java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
+/home/avasileva/project/variants/db/gnomad/gnomad.genomes.v4.1.sites.chrX.vcf.bgz {} > \
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp
+mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/{}
+"
 
 java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate \
 dbSnp132.vcf \
