e91f613a4668890307923646470a589e3b0f2ce2
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-11-01 09:45:06 +0300

    Update vcf_annotating.sh

--- vcf_annotating.sh
+++ vcf_annotating.sh
@@ -67,20 +67,10 @@ ls *.vcf |
 parallel -j 2 "\
 java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
  /home/avasileva/project/variants/db/dbsnp/00-All.vcf {} > \
-/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp/dbsnp_{.}.ann.vcf
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp/dbsnp_{}
 "
 
--rw-rw-r-- 1 avasileva avasileva         0 Oct 27 13:53 dbsnp_clinvar_filtered_HG00096.haplotypeCalls.er.raw.ann.ann.vcf
--rw-rw-r-- 1 avasileva avasileva         0 Oct 27 13:53 dbsnp_clinvar_filtered_HG00101.haplotypeCalls.er.raw.ann.ann.vcf
-
-
 mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad
-
-
-java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
-/home/avasileva/project/variants/db/gnomad/gnomad.genomes.v4.1.sites.chr1.vcf.gz \
-dbsnp_clinvar_filtered_HG00096.haplotypeCalls.er.raw.ann.ann.vcf > \
-temp
 
 ls *.vcf |
 parallel -j 2 "\
@@ -102,16 +92,4 @@ java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
 /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp
 mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/{}
 "
-
-
-java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate \
-dbSnp132.vcf \
-variants.vcf > \
-variants_annotated.vcf
-
-
-# последние версии gnomad, ClinVar, dnsnp.
-java -jar SnpSift.jar annotate dbSnp132.vcf variants.vcf > variants_annotated.vcf
-java -jar SnpSift.jar annotate db/dbSnp/dbSnp137.20120616.vcf test.chr22.vcf
-
 
