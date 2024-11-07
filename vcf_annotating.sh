9ae1e5868ec9cf2c20f332b9b0a8a3f4cbb20acf
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-11-07 11:51:15 +0300

    Update vcf_annotating.sh

--- vcf_annotating.sh
+++ vcf_annotating.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 
+### Annotating variants in SE enhancer and spacer elements, typical enhancers and negative control 
+### using SnpEff (hg38) and SnpSift (gnomad, clinvar, dbsnp) tools. 
+### For SnpSift annotation gnomad, clinvar, dbsnp databases were downloaded separatly. 
+
+# Creating a working directory 
 mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff
 cd /home/avasileva/project/variants/vcf_1000_genomes_filtered_formatted
 
-
-# SnpEff
+# SnpEff annotation
 screen -S annotation
 ls *.vcf |
-parallel -j 50 "\
+parallel -j 50 --plus "\
 java -Xmx8g -jar /home/avasileva/programs/snpEff/snpEff.jar \
 -c /home/avasileva/programs/snpEff/snpEff.config \
 -stats snpeff_{.} \
 -v hg38 {} > \
 /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated/{.}.ann.vcf &&
 "
-# разнести доп поля и vcf файл в разные файлы во время аннотации а потом объединить
 
 
-# downloading dbs
-# gnomad
+# Downloading dbs
+mkdir -p /home/avasileva/project/variants/db
+
+## dbsnp
+# https://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/ file 00-All.vcf.gz 
+mkdir -p /home/avasileva/project/variants/db/dbsnp
+cd  /home/avasileva/project/variants/db/dbsnp
+wget https://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/00-All.vcf.gz
+
+## clinvar
+# https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/ file clinvar.vcf.gz  
+mkdir -p /home/avasileva/project/variants/db/clinvar
+cd /home/avasileva/project/variants/db/clinvar
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz
+
+## gnomad
+# https://gnomad.broadinstitute.org/data Genomes
+mkdir -p /home/avasileva/project/variants/db/gnomad
+cd /home/avasileva/project/variants/db/gnomad
+
 screen -S gnomad
 wget https://storage.googleapis.com/gcp-public-data--gnomad/release/4.1/vcf/genomes/gnomad.genomes.v4.1.sites.chr1.vcf.bgz;
 wget https://storage.googleapis.com/gcp-public-data--gnomad/release/4.1/vcf/genomes/gnomad.genomes.v4.1.sites.chr2.vcf.bgz;
@@ -45,7 +66,7 @@ wget https://storage.googleapis.com/gcp-public-data--gnomad/release/4.1/vcf/geno
 wget https://storage.googleapis.com/gcp-public-data--gnomad/release/4.1/vcf/genomes/gnomad.genomes.v4.1.sites.chrX.vcf.bgz;
 wget https://storage.googleapis.com/gcp-public-data--gnomad/release/4.1/vcf/genomes/gnomad.genomes.v4.1.sites.chrY.vcf.bgz
 
-
+# Indexing gnomad files
 ls *.gz |
 parallel -j 10 "\
 bcftools index -t --threads 9 {}"
