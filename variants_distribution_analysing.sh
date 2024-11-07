649802c9a8cc410617884d82769efc66cbbf841e
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-11-07 11:55:39 +0300

    Update variants_distribution_analysing.sh

--- variants_distribution_analysing.sh
+++ variants_distribution_analysing.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#!/bin/bash
-
 # разбить общую аннотацию на куски по 10 нуклеотидов
 bedops --chop 10 /home/avasileva/project/genome_ann/bg/bg_sampled.bed > /home/avasileva/project/genome_ann/bg/bg_sampled_chopped.bed;
 bedops --chop 10 /home/avasileva/project/monocytes/se/se_e_filtered.bed > /home/avasileva/project/monocytes/se/se_e_filtered_chopped.bed;
 bedops --chop 10  /home/avasileva/project/monocytes/enhancers/te.bed >  /home/avasileva/project/monocytes/enhancers/te_chopped.bed
 
-# counting number of variants per 10 np
+# Сounting number of variants per chopped region
 mkdir -p /home/avasileva/project/variants/variant_distribution/
 cd /home/avasileva/project/variants/vcf_1000_genomes_filtered_no_no_ref_fields_header
 ls *.vcf |
@@ -40,7 +38,3 @@ bedtools intersect \
 # transferring files from server to a local pc
 scp -i '/home/anastasia/Downloads/avasileva.txt' \
 -r avasileva@51.250.11.65:~/project/variants/variant_distribution/ '/home/anastasia/Documents/SE'
-
-
-найти корреляцию спирмана по пациетам
-
