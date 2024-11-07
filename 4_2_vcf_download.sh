a71598ba358b033c483808dbf6a9f03ddff483d8
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-11-07 17:16:54 +0300

    Update 4_2_vcf_download.sh

--- 4_2_vcf_download.sh
+++ 4_2_vcf_download.sh
@@ -12,3 +12,6 @@ wget -i download_links_vcf_1000Genomes30xGRCh38.csv
 
 # exploring downloaded files
 zcat HG00096.haplotypeCalls.er.raw.vcf.gz | head -10000
+
+# readme
+printf "download_links_vcf_1000Genomes30xGRCh38.csv - file containing download links for selected 100 EUR male samples \n" >> readme.txt
