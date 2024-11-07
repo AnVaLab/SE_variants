8b5516c2d27e921a583f833b62637c356f496c8a
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-11-07 14:46:31 +0300

    Update 3_2_monocytes_se_annotation_filtering.sh

--- 3_2_monocytes_se_annotation_filtering.sh
+++ 3_2_monocytes_se_annotation_filtering.sh
@@ -10,3 +10,8 @@ grep "CD14-positive_monocyte" /home/avasileva/project/genome_ann/sea/SE_only_SEA
 # investigating the file
 cat se.bed | wc -l
 # 1168
+
+# Readme file
+touch readme.txt
+printf "This Readme.txt contains description of files in /home/avasileva/project/monocytes/se folder \n \
+se.bed - file containing SE coordinates for monocyte cell type (achieved by SEA filtration)\n" > readme.txt
