d0b4f123776c70ce22c5a0038ffaf1eb89fdfae3
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-11-08 14:33:11 +0300

    Update 4_5_vcf_annotating.sh

--- 4_5_vcf_annotating.sh
+++ 4_5_vcf_annotating.sh
@@ -4,11 +4,9 @@
 ### using SnpEff (hg38) and SnpSift (gnomad, clinvar, dbsnp) tools. 
 ### For SnpSift annotation gnomad, clinvar, dbsnp databases were downloaded separatly. 
 
-# Creating a working directory 
+## 1. SnpEff annotation
+cd /home/avasileva/project/variants/vcf_1000_genomes_filtered_no_regulatory_elem_info_header
 mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff
-cd /home/avasileva/project/variants/vcf_1000_genomes_filtered_formatted
-
-# SnpEff annotation
 screen -S annotation
 ls *.vcf |
 parallel -j 50 --plus "\
@@ -16,48 +14,55 @@ java -Xmx8g -jar /home/avasileva/programs/snpEff/snpEff.jar \
 -c /home/avasileva/programs/snpEff/snpEff.config \
 -stats snpeff_{.} \
 -v hg38 {} > \
-/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated/{.}.ann.vcf &&
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff/snpeff_{.}.ann.vcf &&
 "
 
-# SnpSift
-mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar
+## 2. SnpSift annotation
+# 2.1 clinvar
+cd /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff
+mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar
 screen -S annotation
 ls *.vcf |
 parallel -j 50 "\
 java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate \
  /home/avasileva/project/variants/db/clinvar/clinvar.vcf {} > \
-/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar/clinvar_{.}.ann.vcf
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar/clinvar_{}
 "
 
-mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp
+# 2.2 dbsnp
+cd /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar
+mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp
 screen -S annotation
 ls *.vcf |
 parallel -j 2 "\
 java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
  /home/avasileva/project/variants/db/dbsnp/00-All.vcf {} > \
-/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp/dbsnp_{}
-"
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp/dbsnp_{}"
 
-mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad
-
+# 2.3 gnomad
+cd /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp/
+mkdir -p /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad
 ls *.vcf |
 parallel -j 2 "\
 for i in {1..22};
 do
 java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
 /home/avasileva/project/variants/db/gnomad/gnomad.genomes.v4.1.sites.chr${i}.vcf.gz {} > \
-/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp
-mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/{}
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/temp_{} && \
+mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/temp_{} /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/{}
 done
 
 java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
 /home/avasileva/project/variants/db/gnomad/gnomad.genomes.v4.1.sites.chrX.vcf.gz {} > \
-/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp
-mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/{}
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/temp_{} && \
+mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/temp_{} /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/{}
 
 java -jar /home/avasileva/programs/snpEff/SnpSift.jar annotate -v \
 /home/avasileva/project/variants/db/gnomad/gnomad.genomes.v4.1.sites.chrX.vcf.gz {} > \
-/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp
-mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/temp /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpsift_clinvar_dbsnp_gnomad/{}
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/temp_{} && \
+mv /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/temp_{} /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar_dbsnp_gnomad/{}
 "
 
+## 3. Removing unneeded folders
+rm -rf /home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff \
+/home/avasileva/project/variants/vcf_1000_genomes_filtered_annotated_snpeff_snpsift_clinvar
