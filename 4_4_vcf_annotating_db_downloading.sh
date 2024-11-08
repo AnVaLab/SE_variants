04bc8a166d9e9ba66c4172ea238151753205202e
AnVaLab <anastasia.vasileva.work@gmail.com>
2024-11-08 13:57:02 +0300

    Update 4_4_vcf_annotating_db_downloading.sh

--- 4_4_vcf_annotating_db_downloading.sh
+++ 4_4_vcf_annotating_db_downloading.sh
@@ -2,23 +2,25 @@
 
 ### Downloading dbSNP, clinvar, gnomad dbs for variant annotation with SnpSift tool
 
-# Creating a working directory
+## 1. Creating a working directory
 mkdir -p /home/avasileva/project/variants/db
 
-## dbsnp
+## 2. dbsnp
 # https://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/ file 00-All.vcf.gz 
 mkdir -p /home/avasileva/project/variants/db/dbsnp
 cd  /home/avasileva/project/variants/db/dbsnp
 wget https://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/00-All.vcf.gz
 
-## clinvar
+## 3. clinvar
 # https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/ file clinvar.vcf.gz  
 mkdir -p /home/avasileva/project/variants/db/clinvar
 cd /home/avasileva/project/variants/db/clinvar
 wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz
 
-## gnomad
+## 4. gnomad
 # https://gnomad.broadinstitute.org/data Genomes
+
+# 4.1 downloading gnomad files
 mkdir -p /home/avasileva/project/variants/db/gnomad
 cd /home/avasileva/project/variants/db/gnomad
 
@@ -48,7 +50,12 @@ wget https://storage.googleapis.com/gcp-public-data--gnomad/release/4.1/vcf/geno
 wget https://storage.googleapis.com/gcp-public-data--gnomad/release/4.1/vcf/genomes/gnomad.genomes.v4.1.sites.chrX.vcf.bgz;
 wget https://storage.googleapis.com/gcp-public-data--gnomad/release/4.1/vcf/genomes/gnomad.genomes.v4.1.sites.chrY.vcf.bgz
 
-# Indexing gnomad files
+# 4.2 Indexing gnomad files
 ls *.gz |
 parallel -j 10 "\
 bcftools index -t --threads 9 {}"
+
+## 5. readme
+printf "\
+db folder - contains vcf files of following databases: dbsnp, clinvar, gnomad; \n" >> \
+~/project/variants/readme.txt
